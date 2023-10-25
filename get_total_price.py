31d1b1105c795455ef73ec8ccb5c94f33c792400
dostonradjabov <radjabovdoston0809@gmail.com>
2023-10-25 18:31:51 +0500

    narxi

--- get_total_price.py
+++ get_total_price.py
@@ -7,6 +7,15 @@ def get_total_price(data:str)->float:
     returns:
         float: fruits total price
     """
-    return
+    from csv import reader
+    f = open(data)
+    f = reader(f)
+    c = 0
+    for i in f:
+        if c == 0:
+            c += 1
+            continue
+        print(i[1]) 
+print(get_total_price("fruits.csv"))
 
     
