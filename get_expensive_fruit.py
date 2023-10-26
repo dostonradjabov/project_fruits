b1be3070f901e2402cad54e15010ea5c85057678
dostonradjabov <radjabovdoston0809@gmail.com>
2023-10-26 09:47:51 +0500

    code

--- get_expensive_fruit.py
+++ get_expensive_fruit.py
@@ -8,6 +8,17 @@ def get_expensive_fruit(data: str) -> str:
         str: name of the most expensive fruit
     """
     # your code here
-    return  
+    data  = data.split()
+    mx = 0
+    for i in data[1:]:
+        p = float(i.split(",")[1])
+        n = i.split(",")[0]
+        if mx<p:
+            k = n
+            mx = p
+    return k
+data = open("fruits.csv").read()
+print(get_expensive_fruit(data))
+
 
 
